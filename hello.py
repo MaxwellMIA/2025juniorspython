0b2ff9607e7f7e619819ea595f73b57bfeddc136
Maxwell <cte-berry.gavigan@sllboces.org>
2025-05-06 17:48:26 +0000

    hello

--- hello.py
+++ hello.py
@@ -1,3 +1,8 @@
 # create a variable
 name = input('PLease enter your first name: ')
 print('hello ' + name + '!')
+age = input("what is your age: ")
+print(name + " is " + age + " years old!")
+pet = input("What is your pets name: ")
+print(name + " loves his " + pet + "!")
+print(name + " is " + age + " years old and loves their pet " + pet)
